52dcca6b8fabe2e5c64ee03570ce463be401ecd4
PavloDrabchuk <ravluk2000@gmail.com>
2021-01-29 16:41:29 +0200

    update admin dashboard

--- resources/views/dashboard/page_container.blade.php
+++ resources/views/dashboard/page_container.blade.php
@@ -5,7 +5,14 @@
     <!-- HEADER DESKTOP-->
 
     <!-- MAIN CONTENT-->
-@include('dashboard.main_content')
+{{--@include('dashboard.main_content')--}}
+    <div class="main-content">
+        <div class="section__content section__content--p30">
+            <div class="container-fluid">
+@yield('content')
+            </div>
+        </div>
+    </div>
     <!-- END MAIN CONTENT-->
 
 </div>
