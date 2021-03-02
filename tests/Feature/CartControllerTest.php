ee4dfe46087a5484bdc6b4a0f7c7d43ed00c7edf
PavloDrabchuk <ravluk2000@gmail.com>
2021-03-02 15:35:26 +0200

    some update

--- tests/Feature/CartControllerTest.php
+++ tests/Feature/CartControllerTest.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\CartController;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
